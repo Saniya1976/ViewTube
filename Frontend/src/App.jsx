9fa8eb412637bf70f528ddb945e3b46f1576820e
Saniya1976 <saniyakumari1976@gmail.com>
2025-06-24 18:34:13 +0530

    Initial commit with backend and frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios';
+import {useEffect} from 'react';
+
+function App() {
+   const [jokes,setJokes]=useState([]);
+  return (
+   
+    <>
+      <h1>Hello World</h1>
+      <h2>jokes:{jokes.length}</h2>
+      {
+        jokes.map((joke,index)=>{
+          <div>
+            <h3 key={index}>{joke.joke}</h3>
+            <p key={index}>{joke.id}</p>
+          </div>
+        })
+      }
+    </>
+  )
+}
+
+export default App
