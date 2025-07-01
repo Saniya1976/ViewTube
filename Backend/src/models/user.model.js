673178c4856668fd3e7f14c9afb2a5ec3705f5f4
Saniya1976 <saniyakumari1976@gmail.com>
2025-07-01 15:04:08 +0530

    models

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,7 +1,63 @@
 import mongoose from 'mongoose';
 
 const userSchema=new mongoose.Schema({
-    name:{}
+    Username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20,
+        index: true // Create an index for faster lookups   
+    },
+
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        trim: true,
+        minlength: 6,
+        select: false, // Exclude password from queries by default
+
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+   watchhistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video'
+    }],
+    refreshToken: {
+        type: String,
+        default: ''
+    },
+    avatar: {
+        type: String,
+        default: 'https://res.cloudinary.com/dz1qj3x7h/image/upload/v1735681234/avatars/default-avatar.png',
+        required: true
+    },
 })
 
 const User=mongoose.model('User',userSchema);
--- Backend/src/models/video.model.js
+++ Backend/src/models/video.model.js
@@ -1,6 +1,55 @@
 import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
-    title: {}
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 100,
+        index: true // Create an index for faster lookups
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 500 // Limit description length
+    },
+    duration: {
+        type: Number,
+        required: true,
+        min: 0 // Duration should be a non-negative number
+
+    },
+    videoUrl: {
+        type: String,
+        required: true
+
+    },
+    thumbnailUrl: {
+        type: String,
+        required: true
+    },
+    uploader: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    ispublished: {
+        type: Boolean,
+        default: false
+    },
 });
 const Video=mongoose.model('Video',videoSchema);
